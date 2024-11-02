7c8e24e6cb0421a001b07b357a0bf381fac61a26
Khánh Hòa Lê Phạm <khanhhoa.lp@gmail.com>
2024-11-02 18:59:00 +0700

    add Cors UserService

--- dotNetRetailSystem/RS.UserService/Program.cs
+++ dotNetRetailSystem/RS.UserService/Program.cs
@@ -42,6 +42,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseCors(policy => policy
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
 // Configure the HTTP request pipeline.
 app.MapCarter();
 
